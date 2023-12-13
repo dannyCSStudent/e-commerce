8445f44229df2085f0fbc6a931c5eb26bf92d39d
DEE <danv8605@gmail.com>
2023-12-13 14:13:52 -0600

    fix create-account again

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -18,10 +18,10 @@ export default async function CreateAccount() {
   return (
     <section className={classes.createAccount}>
       <div className={classes.heroImg}>
-        <Link href='/'>
+        <Link href="/">
           <Image 
-            src='/logo-black.svg'
-            alt='logo'
+            src="/logo-black.svg"
+            alt="logo"
             width={250}
             height={23}
             className={classes.logo}
@@ -33,7 +33,7 @@ export default async function CreateAccount() {
           <RenderParams className={classes.params} />
           <div className={classes.formTitle}>
             <h3>Create Account</h3>
-            <Image src='/assets/icons/hand.png' alt='hand' width={30} height={30} />
+            <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
           </div>
           <p>Please enter details</p>
           <CreateAccountForm />
